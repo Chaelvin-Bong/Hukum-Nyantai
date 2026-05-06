1152e861fd88596ef257405d61272e91614e0e98
Chaelvin-Bong <bongchaelvin@gmail.com>
2026-05-06 23:29:42 +0700

    artikel & search bar

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.querySelectorAll('.navbarText').forEach(link => {
     document.querySelector('.navbarGroup').classList.remove('active');
   });
 });
+
+const searchInput = document.getElementById('searchInput');
+const cards = document.querySelectorAll('.card');
+
+searchInput.addEventListener('keyup', function () {
+  const keyword = this.value.toLowerCase();
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+
+    if (text.includes(keyword)) {
+      card.parentElement.style.display = 'block'; // show
+    } else {
+      card.parentElement.style.display = 'none'; // hide
+    }
+  });
+});
